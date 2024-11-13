a74f27f9bb827e1958e27639eef669cfd10cda5b
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-11-13 13:17:39 -0500

    hola

---
+++ src/main/java/ec/edu/ec/welintonsaccon2020/Welintonsaccon2020.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.welintonsaccon2020;
+
+/**
+ *
+ * @author HP
+ */
+public class Welintonsaccon2020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
