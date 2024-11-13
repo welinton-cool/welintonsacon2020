71100da766f80c64d969d894c09e56ea2a351e8f
welinton-cool <welinton.sacon@espoch.edu.ec>
2024-11-13 14:47:52 -0500

    hola

--- src/main/java/ec/edu/ec/welintonsaccon2020/Welintonsaccon2020.java
+++ src/main/java/ec/edu/ec/welintonsaccon2020/Welintonsaccon2020.java
@@ -4,6 +4,8 @@
 
 package ec.edu.ec.welintonsaccon2020;
 
+import java.util.Scanner;
+
 /**
  *
  * @author HP
@@ -11,6 +13,37 @@ package ec.edu.ec.welintonsaccon2020;
 public class Welintonsaccon2020 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    
+        float promedioPrecio;
+        float promedioAlto;
+        float promedioBajo;
+        int contadorPrecio;
+     scanner sc = new Scanner (source: systen.in);
+     for (int i=0; i<7; i++);
+        System.out.println("por favor ingrese la puntuacion");
+        promedioDepuntuaciones
+                
+                
+                
+                
+                
+                
+                puntuaciones.IngresadasPuntuaciones();
+                
+                int opcion;
+                
+                do{
+                    System.out.println("calcular la puntuacion");
+                    System.out.println("1.calcular el total de las puntusciones de las semana");
+                    System.out.println("calcular el promendio de las puntuaciones ");
+                    System.out.println("");
+                    System.out.println("");
+                }
+     
+        
+     
     }
-}
+        
+    }
+
+S
